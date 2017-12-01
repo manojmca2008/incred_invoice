28fd556c67ea60ad7aba32b1bcea2427a68b16f7
Manoj Singhal <manoj841922@gmail.com>
2017-12-01 16:37:51 +0530

    changes

--- src/Containers/ForgetPassword/ForgetPassword.js
+++ src/Containers/ForgetPassword/ForgetPassword.js
@@ -1,48 +1,71 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { forgotPassword } from '../../Services/AuthServices';
 
 class ForgetPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      firebase_forget_error: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  handleClick(e) {
+    e.preventDefault();
+    if (this.state.email === '') {
+      this.setState({
+        firebase_forget_error: 'Email is not empty!'
+      });
+    } else {
+      forgotPassword(this.state.email).then(value => {
+        this.setState({
+          firebase_forget_error: 'A link has been sent to your mail for reset your password'
+        });
+
+      }).catch(err => {
+        this.setState({
+          firebase_forget_error: err.message
+        });
+      })
+    }
+  }
+  handleGoBackClick(e) {
+    window.location.reload('/');
+  }
   render() {
     return (
       <div>
-        <p className="_title">Create Your Password</p>
+        <p className="_title">Oh no, You Forgot Your Password?</p>
         <div className="row">
           <div className="col">
-            <form>
-              <div className="form-group">
-                <label>Email address</label>
-                <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" />
-                <p className="mes_error"></p>
-              </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Password" />
-              </div>
-
-              <div className="form-group">
-                <label>Confirm Password</label>
-                <input type="password" className="form-control" placeholder="Password" />
-              </div>
-
-              <div className="form-check _mb30">
-                <label className="form-check-label">
-                  <input type="checkbox" className="form-check-input" />
-                  Remember Me
-  </label>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-              <Link to="/sign-in" className="btn">Back</Link>
-            </form>
+          <div className="form-group">
+                    <input
+                        className="form-control"
+                        placeholder="E-mail"
+                        name="email"
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleInputChange} />
+                </div>
+                <div className="error">
+                    {this.state.firebase_forget_error}
+                </div>
+            <button type="submit" className="btn btn-primary" onClick={(event) => this.handleClick(event)}>Submit</button>
+            <Link to="/sign-in" className="btn" onClick={(event) => this.handleGoBackClick(event)}>Back</Link>
           </div>
-          <div className="col">
-            <p><Link to="/sign-in">login</Link></p>
-            <p>Not registered yet, Register Now</p>
-            <p><Link to="/sign-up">Sign Up</Link></p>
-          </div>
-
         </div>
       </div>
-
     );
   }
 }
