ac8cf47a974b95af8da5e2c158aac8b6215a84d8
Manoj Singhal <manoj841922@gmail.com>
2017-12-13 08:56:18 +0530

    changes

--- src/Containers/SignIn/SignIn.js
+++ src/Containers/SignIn/SignIn.js
@@ -63,7 +63,7 @@ class SignIn extends Component {
       login(email, password).then(response => {
         let data = {
           email: email,
-          password:password
+          password:password   
         }
         signin(data).then(response => {
           if(response.result){
