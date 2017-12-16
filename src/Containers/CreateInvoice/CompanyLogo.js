d676e4a4fe77ca2092b628add49eb35b5e207662
Arpitg <arpitg396@gmail.com>
2017-12-16 10:07:31 +0530

    ui change

--- src/Containers/CreateInvoice/CompanyLogo.js
+++ src/Containers/CreateInvoice/CompanyLogo.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
 class CompanyLogo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      file: ''
+    };
+
+    this.InputHandler = this.InputHandler.bind(this);
+
+  }
+
+  InputHandler(e) {
+    const files = e.target.files;
+    console.log(files[0].name);
+  }
+
   render() {
     return (
       <div className="popup_addlogo">
@@ -8,7 +24,12 @@ class CompanyLogo extends Component {
         <div className="text-center space">
           <div className="frame_logo d-flex align-items-center justify-content-center">
             ADD LOGO
-            <span className="i_add">+</span>
+            <div className="i_add">
+              <span className="btn_upload">+
+              <input type="file" name="files" className="file-input" value={this.state.value} onChange={this.InputHandler} />
+              
+              </span>
+            </div>
           </div>
           <p className="txt_upload">LOGO IS BEST UPLOADED AS A TRANSPARENT PNG IMAGE</p>
           <a href="javascript:void(0)" className="btn btn-primary l">Save</a>
